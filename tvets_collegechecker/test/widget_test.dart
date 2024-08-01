9db9ccb4e9a2090cc3117bf2be90228e7ee9e0c2
Eulenda <ThembaniNG1_12@geeks4learning.com>
2024-08-01 13:33:37 +0200

    new implentation

--- tvets_collegechecker/test/widget_test.dart
+++ tvets_collegechecker/test/widget_test.dart
@@ -1,22 +1,30 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 import 'package:supabase/supabase.dart';
-import 'package:tvets_collegechecker/main.dart';
+import 'package:tvets_collegechecker/main.dart'; // Ensure this matches your project structure
 
-// Creating a mock class for SupabaseClient using Mockito
 class MockSupabaseClient extends Mock implements SupabaseClient {}
 
 void main() {
-  // Create a mock instance of SupabaseClient
-  final mockSupabaseClient = MockSupabaseClient();
-
   testWidgets('MyApp loads with initial UI', (WidgetTester tester) async {
-    // Build our app and trigger a frame with the mock SupabaseClient.
-    await tester.pumpWidget(MyApp(supabaseClient: mockSupabaseClient));
+    final mockSupabaseClient = MockSupabaseClient();
 
-    // Verify the app loads a widget that includes the text 'Enter college name'
-    expect(find.text('Enter college name'), findsOneWidget);
+    // Wrap MyApp in a provider that provides the mock SupabaseClient
+    await tester.pumpWidget(
+      Provider<SupabaseClient>.value(
+        value: mockSupabaseClient,
+        child: MaterialApp(
+          home: MyApp(
+              supabaseClient:
+                  mockSupabaseClient), // Ensure MyApp is a widget that accepts the context
+        ),
+      ),
+    );
 
+    // Verify the app loads a widget that includes the text 'Enter South African TVET college name'
+    expect(find.text('Enter South African TVET college name'), findsOneWidget);
     // Assuming you have a button to check college
     expect(find.text('Check'), findsOneWidget);
   });
